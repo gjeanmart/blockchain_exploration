d388830b1f766d5368aa2f8f17b31f7cd4327e7e
gjeanmart <gregoire.jeanmart@gmail.com>
2016-08-28 17:40:34 +0000

    added MessageHistory project

---
+++ ethereum/dapp/MessageHistory/contracts/HelloWorld.sol
@@ -0,0 +1,62 @@
+contract MessageHistory {	
+
+	// *********************************************
+	// * Struct 
+    	struct Message {
+        	address sender;
+        	string  text;
+        	uint    date;
+    	}
+
+	
+	// *********************************************
+	// * Data 
+	address public owner;
+	Message[] public messageHistory;
+	
+	// *********************************************
+	// * Modifier
+    	modifier onlyOwner {
+        	if (msg.sender != owner) throw;
+        	_
+    	}
+	
+	// *********************************************
+	// * Constructor 
+	// @description
+	function MessageHistory() {
+		owner 		= msg.sender;
+	}
+	
+	// *********************************************
+	// * sendMessage 
+	// @description
+	function sendMessage(string _text) returns (bool sucess) {
+		Message memory msg;
+		
+		msg.sender 	= msg.sender;
+		msg.text 	= _text;
+		msg.date 	= now; 
+
+		messageHistory.push(msg);
+		
+		return true;
+	}
+	
+	// *********************************************
+	// * getLastMessage 
+	// @description
+	function getLastMessage() returns (address, string, uint) {
+		uint i = messageHistory.length - 1;
+
+		return (messageHistory[i].sender, messageHistory[i].text, messageHistory[i].date);
+	}
+
+	// *********************************************
+	// * kill 
+	// @description: Function to recover the funds on the contract
+    	function kill() onlyOwner { 
+        	selfdestruct(owner); 
+    	}
+
+}
