f19e09c3d6b24c5e030a0517427777378e9a4f4a
gjeanmart <gregoire.jeanmart@gmail.com>
2016-08-29 20:05:05 +0000

    some changes

--- ethereum/dapp/MessageHistory/truffle.js
+++ ethereum/dapp/MessageHistory/truffle.js
@@ -12,5 +12,28 @@ module.exports = {
   rpc: {
     host: "localhost",
     port: 8545
+  },
+  networks: {
+    "live": {
+      network_id: 1, // Ethereum public network
+      // optional config values
+      // host - defaults to "localhost"
+      // port - defaults to 8545
+      // gas
+      // gasPrice
+      // from - default address to use for any transaction Truffle makes during migrations
+    },
+    "morden": {
+      network_id: 2,        // Official Ethereum test network
+      host: "localhost",    // Random IP for example purposes (do not use)
+      port: 8546             
+    },
+    "staging": {
+      network_id: 1337 // custom private network
+      // use default rpc settings
+    },
+    "dev": {
+      network_id: "default"
+    }
   }
 };
